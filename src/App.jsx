c6aa6e0dab0b7b477fc93e70fc02b1fa4de85bad
Nil Farras <farrasnil@gmail.com>
2024-05-17 15:46:50 +0200

    CRUD test done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-
+import { useEffect } from "react";
+import  notesServices  from "./services/notes/notesServices.js";
 
 function App() {
 
+  useEffect(() => {
+    notesServices
+      .getNotes()
+      .then((notes)=>{
+        console.log("Tus ntas",notes)
+      });
+  }, []);
+
   return
+
+
 }
 
 export default App
